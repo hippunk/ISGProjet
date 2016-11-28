9c8bafe1432b91a607f58bf287a4caf5f63ab4fd
Janini <porlonjanice@live.fr>
2016-11-28 18:08:37 +0100

    Ajout de l'animation de mort pour tous ceux ayant le component Life

---
+++ Assets/Components/Life.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+	public float life = 10.0f;
+}
---
+++ Assets/Systems/AnimationSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using FYFY;
+
+public class AnimationSystem : FSystem {
+
+	private Family _controllableGO = FamilyManager.getFamily(
+		new AllOfComponents(typeof(Life)));
+
+	protected override void onProcess(int familiesUpdateCount){
+		foreach (GameObject go in _controllableGO) {
+			Color col = go.GetComponent<Renderer> ().material.color;
+			//Lorsqu'un organisme meurt, déclenchement d'une animation pour qu'il disparaisse progressivement
+			if (go.GetComponent<Life> ().life <= 0 && col.a > 0) {
+				col.a -= 0.01f;
+				go.GetComponent<Renderer> ().material.color = col;
+			}
+
+			//Destruction du gameObject lorsqu'il n'est plus visible
+			if (col.a <= 0)
+				GameObjectManager.destroyGameObject (go);
+		}
+	}
+}
