90772dc14a26f29bdf8b975de9da8f37dbed308b
Daphné HB <daphne.hbek@gmail.com>
2016-11-30 21:30:12 +0100

    Ajout d'une scene du debut de l'interface (sceneAvecBarre)

---
+++ Assets/Systems/AfficheInfos.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using FYFY;
+using FYFY_plugins.MouseManager;
+
+public class AfficheInfos : FSystem {
+
+	private Family _entitesGO = FamilyManager.getFamily(
+		new AllOfComponents(typeof(MouseOver))); // NOTE : changer components pour traiter tous les enemis
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+		foreach (GameObject go in _entitesGO) {
+			Debug.Log ("Afficher sa vie, nom, ...");
+		}
+	}
+}
