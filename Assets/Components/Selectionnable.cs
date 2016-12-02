f1aa54bd0c78441af061a35083a71703f0a6a11c
hippunk <arthur.ramolet@hotmail.fr>
2016-12-02 04:28:47 +0100

    push oubli

--- Assets/Components/Selectionnable.cs
+++ Assets/Components/Selectionnable.cs
@@ -4,4 +4,5 @@ public class Selectionnable : MonoBehaviour {
     // Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
     public Sprite sprite;
 	// Le point de la map vers lequel la défense se déplace
+	public Vector3 goalPosition = Vector3.zero;
 }
